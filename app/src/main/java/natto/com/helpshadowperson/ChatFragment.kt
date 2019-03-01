5b44599068634d5bf1b0eabfa77d574dc603e722
TaigaNatto <taigan3253@gmail.com>
2019-03-01 13:26:42 +0900

    fix

--- app/src/main/java/natto/com/helpshadowperson/ChatFragment.kt
+++ app/src/main/java/natto/com/helpshadowperson/ChatFragment.kt
@@ -20,15 +20,39 @@ class ChatFragment : Fragment() {
 
         context?.let {
             adapter = ChatAdapter(it)
-            adapter.messageAdd(ChatMessage(true, "", "しりとりしよう！", "", ""))
-            for (i in 0..20) {
-                adapter.messageAdd(ChatMessage(false, "", "トンボ", "", ""))
-                adapter.messageAdd(ChatMessage(true, "", "ボルト", "", ""))
-            }
+//            adapter.messageAdd(ChatMessage(true, "", "しりとりしよう！", "", ""))
+//            for (i in 0..20) {
+//                adapter.messageAdd(ChatMessage(false, "", "トンボ", "", ""))
+//                adapter.messageAdd(ChatMessage(true, "", "ボルト", "", ""))
+//            }
             binding.listChat.adapter = adapter
         }
         binding.listChat.setSelection(adapter.count)
 
+        binding.editMessage.onFocusChangeListener = View.OnFocusChangeListener { view, isFocus ->
+            if (isFocus) {
+                binding.btnCamera.visibility = View.GONE
+                binding.btnPicture.visibility = View.GONE
+                binding.btnMic.visibility = View.GONE
+                binding.btnUp.setImageResource(R.drawable.ic_send_blue_24dp)
+            } else {
+                binding.btnCamera.visibility = View.VISIBLE
+                binding.btnPicture.visibility = View.VISIBLE
+                binding.btnMic.visibility = View.VISIBLE
+                binding.btnUp.setImageResource(R.drawable.ic_thumb_up_glay_24dp)
+            }
+        }
+
+        binding.btnUp.setOnClickListener {
+            val text = binding.editMessage.text
+            if (binding.editMessage.isFocusable && text.toString().isNotEmpty()) {
+                adapter.messageAdd(ChatMessage(true, "", text.toString(), "", ""))
+                adapter.notifyDataSetChanged()
+                binding.listChat.setSelection(adapter.count)
+
+                binding.editMessage.setText("")
+            }
+        }
         return binding.root
     }
 }
